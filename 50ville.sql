168653e1e1ec21c7dfc354ea80ce18547cfb9900
jse <151858639+jared-easton@users.noreply.github.com>
2025-08-17 11:00:18 -0400

    Add files via upload

---
+++ 50ville.sql
@@ -0,0 +1,12 @@
+select people.phone_number, people.passport_number, people.license_plate from people
+where people.license_plate = "5P2BI95"
+and people.license_plate = "94KL13X"
+and people.license_plate = "6P58WS2"
+and people.license_plate = "4328GD8"
+and people.license_plate = "G412CB7"
+and people.license_plate = "L93JTIZ"
+and people.license_plate = "322W7JE"
+and people.license_plate = "0NTHK55"
+order by people.name asc
+
+
